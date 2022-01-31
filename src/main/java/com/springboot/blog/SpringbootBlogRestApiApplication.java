2f21961f451dd000277729eda5b6cadb40bf2a7c
Snehal Karanje <snehalkaranje9@gmail.com>
2022-01-31 16:27:47 +0530

    Configured MySQL DB

--- src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -2,8 +2,9 @@ package com.springboot.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringbootBlogRestApiApplication {
 
 	public static void main(String[] args) {
