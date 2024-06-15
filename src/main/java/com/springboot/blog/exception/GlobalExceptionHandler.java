e09ef895cb702b1d25e272b935bc9182aa6471d5
Snehal Karanje <snehalkaranje9@gmail.com>
2024-06-15 18:00:44 +0530

    Spring Security: Handle Access Denied Exception

--- src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.springboot.blog.exception;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
+import com.springboot.blog.payload.ErrorDetails;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,7 +13,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import com.springboot.blog.payload.ErrorDetails;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 // internally uses @Component annotation. 
 // So, this class will be auto detected during component scanning
@@ -38,6 +38,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		ErrorDetails errorDetails = new ErrorDetails(HttpStatus.BAD_REQUEST, new Date(), exception.getMessage(), webRequest.getDescription(false));
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.BAD_REQUEST);
 	}
+
+	@ExceptionHandler(AccessDeniedException.class)
+	protected ResponseEntity<ErrorDetails> handleAccessDeniedException(AccessDeniedException exception, WebRequest webRequest) {
+		ErrorDetails errorDetails = new ErrorDetails(HttpStatus.UNAUTHORIZED, new Date(), exception.getMessage(), webRequest.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.UNAUTHORIZED);
+	}
 	
 	// handle global exception
 	// since every exception class extends 'Exception' class,
@@ -47,7 +53,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 		ErrorDetails errorDetails = new ErrorDetails(HttpStatus.INTERNAL_SERVER_ERROR, new Date(), exception.getMessage(), webRequest.getDescription(false));
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			  HttpHeaders headers, HttpStatusCode status, WebRequest request) {
